b3537984c63356c040783aaebb39c8dc1861c5fa
Abraham <abrahammorillo777@gmail.com>
2024-04-28 11:15:57 -0400

    remove unnecesary file

--- src/database/database.ts
+++ src/database/database.ts
@@ -2,7 +2,7 @@ import { DataSource } from "typeorm";
 import { Expense } from "../expenses/domain/expense"; // Import your entities here
 import dotenv from "dotenv";
 import { Users } from "../users/domain/User";
-dotenv.config({ path: "../config.env" });
+dotenv.config({ path: "../../.env" });
 
 export const AppDataSource = new DataSource({
   type: "postgres",
